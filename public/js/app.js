49221f79b9c9da7b48eb2f9636cec65b86675cdb
Vikas Navale S <vikas.shivaraj@mindtree.com>
2019-08-02 14:59:08 +0530

    remove unnecessary consoles

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log('client side js file loaded');
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
