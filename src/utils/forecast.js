e4d7c70d9bdb8e995d71e29bebe0213ac13104f2
Vikas Navale S <vikas.shivaraj@mindtree.com>
2019-08-05 15:58:18 +0530

    add hogh and low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ forecast = (lattitude, longitude, callback) => {
         } else if (body.error){
             callback('unable to find', undefined);
         }else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is ${body.currently.precipProbability}% chance of rain.` );
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. The high today is ${body.daily.data[0].temperatureHigh} with a low of ${body.daily.data[0].temperatureLow}. There is ${body.currently.precipProbability}% chance of rain.` );
         }
     })
 }
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -2,7 +2,7 @@ request = require('request');
 
 const geocode = (address, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoidmlrYXNhbnU5ODciLCJhIjoiY2p5cG5qM2k2MDVtOTNsbGVhdnR4ZWU5NCJ9.pEXqFVkMdVwfZe_rBI9YCw&limit=1`
-    request({ url, json: true}, (error, {body}) => {
+    request({ url, json: true}, (error, {body} = {}) => {
         if (error) {
             callback('Unable to connect to location services', undefined);
         } else if (body.features.length === 0) {
